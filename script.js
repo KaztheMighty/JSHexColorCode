18267e0776736b39fcc0d04b329ab2f6aad8f91d
Ben Pabian <benjamin.pabian@hotmail.com>
2020-11-18 20:59:26 +1100

    Finished most code, still need to error correct

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const hexNos = [0,1,2,3,4,5,6,7,8,9, "A", "B", "C", "D", "E", "F"];
+const hexBtn = document.querySelector(".hexButton");
+const bodyBg = document.querySelector("body");
+const hex = document.querySelector(".hex");
 
+hexBtn.addEventListener('click', getRandHex);
+
+function getRandHex() {
+	var hexCol = "#";
+
+	for (var i = 0; i < 6; i++){
+		var random = Math.floor(Math.random() * hexNos.length);
+		console.log(random);
+
+		hexCol += hexNos[random];
+		console.log(hexCol);
+	}
+
+	bodyBg.style.backgroundColor = hexCol;
+	hex.innerHTML = hexCol;
+}
